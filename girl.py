2fcc3c826f462f21aac97044f8042f13aa176dc7
Martin <martinthecoder@gmail.com>
2017-06-25 02:27:53 -0400

    Update girl.py

--- girl.py
+++ girl.py
@@ -5,6 +5,7 @@ class Girl():
     def __init__(self, screen):
         """Initialize the girl and set its starting position."""
         self.screen = screen
+        
 
         #Load the girl image and get its rect.
         self.image = pygame.image.load ('images/girl.bmp')
@@ -15,6 +16,19 @@ class Girl():
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
 
+        #Store a decimal value for the girl's center.
+        #self.center = float (self.rect.centerx)
+        
+        #Movement flag
+        self.moving_right = False
+        self.moving_left = False
+
+    def update (self):
+        """Update the girl's position based on the movement flag."""
+        if self.moving_right:
+            self.rect.centerx += 1
+        if self.moving_left:
+            self.rect.centerx -= 1
     def blitme(self):
         """Draw the girl at its current position."""
         self.screen.blit (self.image, self.rect)
