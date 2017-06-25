fa191dd4721f75aa285df9b7c1e3a5ca5ad73fca
Martin <martinthecoder@gmail.com>
2017-06-25 02:27:22 -0400

    Update girlbrave.py

--- girlbrave.py
+++ girlbrave.py
@@ -21,6 +21,7 @@ def run_game ():
     #Start the main loop for the game.
     while True:
         gf.check_events (girl)
+        girl.update()
         gf.update_screen(gb_settings, screen, girl)
 
         #Watch for keyboard and mouse events.
