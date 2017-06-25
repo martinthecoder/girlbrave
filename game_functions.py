717c9ab097f7852ef6f60a3d32a002203b5c6437
Martin <martinthecoder@gmail.com>
2017-06-25 02:59:24 -0400

    Update game_functions.py

--- game_functions.py
+++ game_functions.py
@@ -2,22 +2,30 @@ import sys
 
 import pygame
 
+def check_keydown_events (event, girl):
+    """Respond to keypresses"""
+    if event.key == pygame.K_RIGHT:
+        girl.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        girl.moving_left = True
+
+def check_keyup_events (event, girl):
+    """Respond to key releases."""
+    if event.key == pygame.K_RIGHT:
+        girl.moving_right = False
+    elif event.key == pygame.K_LEFT:
+        girl.moving_left = False
+
+    
 def check_events(girl):
     """Respond to keypresses and mouse events."""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                girl.moving_right = True
-            elif event.key == pygame.K_LEFT:
-                girl.moving_left = True
-
+            check_keydown_events (event, girl)
         elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_RIGHT:
-                girl.moving_right = False
-            elif event.key == pygame.K_LEFT:
-                girl.moving_left = False
+            check_keyup_events (event, girl)
 
 def update_screen (gb_settings, screen, girl):
     """Update images on the screen and flip to the new screen."""
