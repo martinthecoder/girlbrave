2815ea080f944bc0e13b079b22d1ad3479a022c1
Martin <martinthecoder@gmail.com>
2017-06-25 01:24:37 -0400

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,20 +1,9 @@
-
-
 class Settings ():
     """A class to store all settings for Girl Brave."""
 
-    def __init__ (self):
-        """Initialize the game settings."""
+    def __init__(self):
+        """Initialize the game's settings."""
         #Screen settings.
-        self.screen_width = 1000
-        self.screen_height = 600
-        self.bg_color = (252, 40, 71)
-
-        #Girl settings
-        self.girl_speed_factor = 1
-
-        #Bullet settings
-        self.bullet_speed_factor = 2
-        self.bullet_width = 3
-        self.bullet_height = 15
-        self.bullet_color = 60, 60, 60
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
